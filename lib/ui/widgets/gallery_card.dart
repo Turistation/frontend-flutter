edb01449795d36ed40c908a222cacaa86fe6971e
arfan <arfan.jadul10@gmail.com>
2022-07-04 16:34:26 +0700

    add layer warna hitam biar tulisan keliatan

--- lib/ui/widgets/gallery_card.dart
+++ lib/ui/widgets/gallery_card.dart
@@ -10,33 +10,57 @@ class GalleryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 156,
-      width: 176,
-      padding: EdgeInsets.only(left: 10, bottom: 16),
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(14),
-        image: DecorationImage(
-            image: NetworkImage(photos.photos), fit: BoxFit.cover),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text(
-            photos.blog.title,
-            style: whiteTextStyle.copyWith(
-                fontSize: 12, fontWeight: FontWeight.w700),
+    return Stack(
+      children: [
+        Container(
+          height: 156,
+          width: 176,
+          padding: EdgeInsets.only(left: 10, bottom: 16),
+          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(14),
+            image: DecorationImage(
+                image: NetworkImage(photos.photos), fit: BoxFit.cover),
           ),
-          Text(
-            photos.category.name,
-            style: whiteTextStyle.copyWith(
-              fontSize: 10,
-            ),
+        ),
+        Container(
+          height: 156,
+          width: 176,
+          padding: EdgeInsets.only(left: 10, bottom: 16),
+          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(14),
+              gradient: LinearGradient(
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  colors: [
+                    Colors.grey.withOpacity(0.0),
+                    Colors.black,
+                  ],
+                  stops: const [
+                    0.0,
+                    1.1
+                  ])),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                photos.blog.title,
+                style: whiteTextStyle.copyWith(
+                    fontSize: 12, fontWeight: FontWeight.w700),
+              ),
+              Text(
+                photos.category.name,
+                style: whiteTextStyle.copyWith(
+                  fontSize: 10,
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
